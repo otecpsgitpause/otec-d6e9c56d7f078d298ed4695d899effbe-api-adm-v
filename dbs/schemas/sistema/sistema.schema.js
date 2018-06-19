554fb55b0ed9db5af3666b6796f0a7f58971cd8a
otecpsgitpause <34405402+otecpsgitpause@users.noreply.github.com>
2018-06-19 17:27:41 -0400

    add sistema
    add sistema

---
+++ dbs/schemas/sistema/sistema.schema.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var modelSchema = new schema({
+    nombre:String,
+    correo:String,
+    motivo:String,
+    leido:Boolean,
+    respondido:Boolean,
+    fecha:String
+})
+
+module.exports = mongoose.model('sistema', modelSchema);
